3fec49e5980803f80f63533752235af8d53a0f63
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2021-02-22 15:59:45 -0600

    added Kiwis and drumpad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
+import Drumpad from './components/Drumpad'
+
+//https://github.com/kevinsqi/react-piano we used this one as the base
+// we didn't use this one, but there is a tutorial for integrating with tone.js https://github.com/lillydinhle/react-piano-component  
 
 function App() {
     const firstNote = MidiNumbers.fromNote('c3');
@@ -12,18 +16,20 @@ function App() {
     });
    
     return (
-      <Piano
-        noteRange={{ first: firstNote, last: lastNote }}
-        playNote={(midiNumber) => {
-          // Play a given note - see notes below
-        }}
-        stopNote={(midiNumber) => {
-          // Stop playing a given note - see notes below
-        }}
-        width={1000}
-        keyboardShortcuts={keyboardShortcuts}
-      />
-    
+      <div>
+        <Drumpad/>
+        <Piano
+          noteRange={{ first: firstNote, last: lastNote }}
+          playNote={(midiNumber) => {
+            // Play a given note - see notes below
+          }}
+          stopNote={(midiNumber) => {
+            // Stop playing a given note - see notes below
+          }}
+          width={1000}
+          keyboardShortcuts={keyboardShortcuts}
+        />
+      </div>
   );
 }
 
