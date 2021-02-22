7106072d6167b71e2ee608f03862bcc80ddaf3b6
Danny Chung <41158853+hachui@users.noreply.github.com>
2021-02-22 18:53:47 -0500

    react piano with sound

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
+import SoundfontProvider from './SoundFontProvider';
+
+// webkitAudioContext fallback needed to support Safari
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+
+const noteRange = {
+  first: MidiNumbers.fromNote('c3'),
+  last: MidiNumbers.fromNote('f4'),
+};
+const keyboardShortcuts = KeyboardShortcuts.create({
+  firstNote: noteRange.first,
+  lastNote: noteRange.last,
+  keyboardConfig: KeyboardShortcuts.HOME_ROW,
+});
 
 function App() {
     const firstNote = MidiNumbers.fromNote('c3');
@@ -12,17 +27,21 @@ function App() {
     });
    
     return (
-      <Piano
-        noteRange={{ first: firstNote, last: lastNote }}
-        playNote={(midiNumber) => {
-          // Play a given note - see notes below
-        }}
-        stopNote={(midiNumber) => {
-          // Stop playing a given note - see notes below
-        }}
-        width={1000}
-        keyboardShortcuts={keyboardShortcuts}
-      />
+      <SoundfontProvider
+      instrumentName="acoustic_grand_piano"
+      audioContext={audioContext}
+      hostname={soundfontHostname}
+      render={({ isLoading, playNote, stopNote }) => (
+        <Piano
+          noteRange={noteRange}
+          width={300}
+          playNote={playNote}
+          stopNote={stopNote}
+          disabled={isLoading}
+          keyboardShortcuts={keyboardShortcuts}
+        />
+      )}
+    />
     
   );
 }
