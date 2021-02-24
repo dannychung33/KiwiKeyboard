6738d1576354557d86fc00d0a1e8275844a92968
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2021-02-24 17:55:00 -0600

    how do make the power button

---
+++ src/components/Power.js
@@ -0,0 +1,10 @@
+import '../App.css'
+
+// power button should activate (and ideally visually "bite" the big kiwi pad) the effects on the sample. 
+const Power = () => {
+    return(
+        <button className='powerButton'>🥝</button>
+    )
+}
+
+export default Power
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import '../App.css'
+import Power from './Power'
+
+const Title = () => {
+    return(
+    <h2 className="piano_title">Kiwi 
+        <Power />
+    Keys</h2>
+    )
+}
+
+export default Title
